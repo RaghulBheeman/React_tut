8d6bed02b29ec9741cf576ad38d8ba612ba80c51
RaghulBheeman <raghulbheeman35@gmail.com>
2023-10-03 01:01:36 +0530

    function for ckeckboxs and delete button has been written

--- src/Content.js
+++ src/Content.js
@@ -4,7 +4,7 @@ import {FaRegTrashAlt} from "react-icons/fa";
 
 const Content = ()=>{
 
-    const [items,setitems]=useState(
+    const [items,setItems]=useState(
         [{
             id:1,
             checked:true,
@@ -22,30 +22,49 @@ const Content = ()=>{
         }])
   
         const handleCheck=(id)=>{
-            
+            const listItems = items.map((item) =>
+            item.id===id  ? {...item, checked:!item.checked} : item)
+            setItems(listItems)
+            localStorage.setItem("todo_list",JSON.stringify
+            (listItems))
+        }
+
+        const handledelete = (id) => {
+            const listItem = items.filter((item) =>
+            item.id!==id)
+            setItems(listItem)
+            localStorage.setItem("todo_list",JSON.stringify
+            (listItem))
         }
 
 
     return(
 
         <main> 
-
-            <ul>
+            {items.length ? (
+            <ul> 
                 {items.map(item=>(
                     <li className="item" key ={items.id}>
                         <input type="checkbox" 
                         onChange={()=>handleCheck(item.id)}
                         checked={item.checked}
                         />
-                        <label>{item.item}</label>
+                        <label style = {(item.checked) ? {textDecoration: 'line-through'} : null}>
+                           {item.item}</label>
                         <FaRegTrashAlt
                             role="button"
+                            onClick={()=>handledelete(item.id)}
                             tabIndex="0"
                         />
 
                     </li>
                 ))}
             </ul>
+
+            ):(
+                <p style={ { marginTop:'270px'}}>Your list is empty</p>
+            )
+            }
         </main>
     )
 }
